0d2020bb5d18f6023a48a75ef7280ce73b8dbd27
Joern Richter <joerich1988@gmail.com>
2021-09-30 10:18:55 +0200

    Bump version to 0.1.3

--- tests/test_dixday_predictions.py
+++ tests/test_dixday_predictions.py
@@ -1,5 +1,15 @@
+import csv
+import yaml
+
 from dixday_predictions import __version__
+from dixday_predictions.eventhandler.EventHandler import EventHandler
+
+
+def _read_config(config_path) -> dict:
+    with open(config_path, "r") as ymlfile:
+        config = yaml.safe_load(ymlfile)
+    return config
 
 
 def test_version():
-    assert __version__ == '0.1.2'
+    assert __version__ == '0.1.3'
